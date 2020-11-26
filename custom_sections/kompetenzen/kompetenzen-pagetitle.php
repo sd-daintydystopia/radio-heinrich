0a91b170793f56a84766e6ce6588f7ae0d7d130b
sd-daintydystopia <74842816+sd-daintydystopia@users.noreply.github.com>
2020-11-26 03:20:42 +0100

    Add files via upload
    Pagetitle & Intro Kompetenzen upload

---
+++ custom_sections/kompetenzen/kompetenzen-pagetitle.php
@@ -0,0 +1,14 @@
+<?php
+	$h1 = 'Kompetenzen';
+	$h2 = 'Unsere';
+
+?>
+<!-- Page Title
+============================================= -->
+    <section id="page-title" class="page-title-parallax" style="background-image: url('images/Fotos/parallax/parallax05.jpg'); background-size: cover; padding: 120px 0;" data-bottom-top="background-position:0px 0px;" data-top-bottom="background-position:0px -300px;">
+
+        <div class="container clearfix">
+            <h2><?php echo $h2; ?></h2>
+            <h1><?php echo $h1; ?></h1>
+        </div>
+    </section><!-- #page-title end -->
