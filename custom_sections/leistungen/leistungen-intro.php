cc7f12c3475f0b78c96e5d223216a470a02bced7
sd-daintydystopia <74842816+sd-daintydystopia@users.noreply.github.com>
2020-11-26 03:21:22 +0100

    Add files via upload

---
+++ custom_sections/leistungen/leistungen-intro.php
@@ -0,0 +1,27 @@
+<?php 
+	$h3 = "KONZENTRIERTES FACHWISSEN, MODERNSTE TECHNIK UND MENSCHLICHE FÜRSORGE.";
+    $h4 = "FÜR IHRE GESUNDHEIT:";
+	$text_block1 = "Das Wohlbefinden unserer Patientinnen und Patienten steht im Mittelpunkt unseres gesamten
+Handelns. Unser Ziel ist die bestmögliche Versorgung, ausgerichtet auf medizinische Anforderungen
+und individuelle Bedürfnisse.";
+	$text_block2 = "Mit der Spezialisierung auf acht Kompetenzzentren bündeln wir zu diesem Zweck die Expertise und
+Erfahrung von Radiologen, Nuklearmedizinern und Strahlentherapeuten. So können wir komplexe
+Krankheitsbilder aus verschiedenen Blickwinkeln bewerten und kombinieren dementsprechend
+Know-how und Technologien - für präzise Diagnostik und patientenorientierte Lösungen.";
+?>
+<section id="content">
+    <div class="content-wrap">
+        <div class="container clearfix">
+            <div class="row">
+                <div class="w-100"></div>
+                <div class="col-8">
+                    <h4><?php echo $h4; ?></h4>
+                    <h3><?php echo $h3; ?></h3>
+                    <p><?php echo $text_block1; ?></p>
+                    <p><?php echo $text_block2; ?></p>
+                </div>
+                <div class="w-100"></div>
+            </div>
+        </div>
+    </div>
+</section>
