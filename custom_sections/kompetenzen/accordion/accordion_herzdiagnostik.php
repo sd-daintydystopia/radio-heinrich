bdbff91c2afbea350c2df0bcfec980041e40d5ab
sd-daintydystopia <74842816+sd-daintydystopia@users.noreply.github.com>
2020-11-26 02:54:51 +0100

    Add files via upload

---
+++ custom_sections/kompetenzen/accordion/accordion_herzdiagnostik.php
@@ -0,0 +1,77 @@
+<?php 
+
+
+?>
+<div id="accordion">
+	<div class="card">
+		<div class="card-header" id="headingOne">
+			<h5 class="mb-0">
+				<button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+				Koronare Herzerkrankung / Herzkranzgefäße
+				</button>
+			</h5>
+		</div>
+
+		<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+			<div class="card-body">
+				Aufgrund der genauen Beurteilbarkeit des Herzmuskels kann man durch die MRT-Untersuchung des Herzens Durchblutungsstörungen bei hochgradigen Einengungen der Herzkranzgefäße bereits darstellen bevor es zu einem Herzinfarkt kommt. Die ist besonders hilfreich, wenn aufgrund von Voruntersuchungen wie zB einem Belastungs-EKG noch Unklarheiten bestehen. Auch nach Erleiden eines Herzinfarkts kann dieser mittels Herz MRT dargestellt werden und beispielsweise bestimmt werden, wie groß der davon betroffene Anteil des Herzens ist. Hilfreich ist dies zB bei der Entscheidung, ob ein nachfolgender Eingriff (Herzkatheter / Herz- Operation) sinnvoll ist, da überprüft werden kann, wieviel Herzmuskel noch zu retten wäre und wieviel unwiederbringlich vernarbt ist.
+			</div>
+		</div>
+	</div>
+	<div class="card">
+		<div class="card-header" id="headingTwo">
+			<h5 class="mb-0">
+				<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+				Myokarditis/Perikarderkrankung
+				</button>
+			</h5>
+		</div>
+		<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+			<div class="card-body">
+				Durch die gute Beurteilbarkeit unterschiedlicher Gewebe, lassen sich Entzündungen des Herzmuskels und auch des Herzbeutels gut mittels MRT-Untersuchung des Herzens nachweisen oder ausschließen.
+			</div>
+		</div>
+	</div>
+	<div class="card">
+		<div class="card-header" id="headingThree">
+			<h5 class="mb-0">
+				<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+				Kardiale und parakardiale Raumforderungen/Tumore
+				</button>
+			</h5>
+		</div>
+		<div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+			<div class="card-body">
+				Falls bereits mit anderen bildgebenden Verfahren Raumforderungen im Bereich des Herzens nachgewiesen wurden oder ein Verdacht besteht, können diese mittels MRT-Untersuchung des Herzens genauer untersucht und abgeklärt werden. Hierbei können unter anderem die exakte anatomische Lage aber auch die Art bzw. der Ursprung der Raumforderung bestimmt werden.
+			</div>
+		</div>
+	</div>
+	<div class="card">
+		<div class="card-header" id="headingThree">
+			<h5 class="mb-0">
+				<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseThree">
+				Erworbene Herzklappenfehler
+				</button>
+			</h5>
+		</div>
+		<div id="collapseF" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+			<div class="card-body">
+				Wichtig für den gerichteten Weitertransport des Blutes durch das Herz ist die regelrechte Funktion der Herzklappen. Kommt es hier zu Funktionsstörungen, kann die Leistungsfähigkeit des Herzens deutlich eingeschränkt sein. In erster Linie werden die Herzklappen mittels Echokardiographie untersucht, allerdings gibt es immer wieder Situationen, in denen sich hierdurch nicht alle Fragen beantworten lassen. In einem solchen Fall kann eine MRTUntersuchung oftmals weitere wichtige Informationen liefern.
+			</div>
+		</div>
+	</div>
+	<div class="card">
+		<div class="card-header" id="headingThree">
+			<h5 class="mb-0">
+				<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseThree">
+				Kardiomyopathien
+				</button>
+			</h5>
+		</div>
+		<div id="collapseFive" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+			<div class="card-body">
+				Es gibt eine Vielzahl an Gründen warum der Herzmuskel nicht oder nicht mehr uneingeschränkt funktions- und Leistungsfähig ist. Beispiele hierfür sind bestimmte Ablagerungen im Herzmuskel, eine übermäßige Verdickung (Hypertrophie) des Muskels oder eine Vergrößerung des Herzens (Dilatation). Eine MRT-Untersuchung des Herzens ist eine hilfreiche Methode, um die mögliche Ursache einer Funktionseinschränkung zu klären oder andere Ursachen auszuschließen.
+			</div>
+		</div>
+	</div>
+</div>
