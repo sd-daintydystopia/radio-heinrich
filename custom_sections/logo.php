63e903f658113ff137622221015bf9bf970c2d71
sd-daintydystopia <74842816+sd-daintydystopia@users.noreply.github.com>
2020-11-29 16:38:49 +0100

    Update logo.php

--- custom_sections/logo.php
+++ custom_sections/logo.php
@@ -1,4 +1,4 @@
 <div id="logo">
-    <a href="index.html" class="standard-logo" data-dark-logo="images/logo-dark.png"><?php echo file_get_contents("images/logos/logo_main.svg"); ?></a>
-    <a href="index.html" class="retina-logo" data-dark-logo="images/logo-dark@2x.png"><img src="images/logo@2x.png" alt="Canvas Logo"></a>
+    <a href="index.php" class="standard-logo" data-dark-logo="images/logo-dark.png"><?php echo file_get_contents("images/logos/logo_main.svg"); ?></a>
+    <a href="index.php" class="retina-logo" data-dark-logo="images/logo-dark@2x.png"><img src="images/logo@2x.png" alt="Logo RADIOLOGIE HEINRICHSALLEE"></a>
 </div><!-- #logo end -->
