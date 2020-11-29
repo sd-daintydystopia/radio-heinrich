000a09ea94d9277070de2a97de675ffcd5de54e0
sd-daintydystopia <74842816+sd-daintydystopia@users.noreply.github.com>
2020-11-29 16:37:50 +0100

    Add files via upload

---
+++ custom_sections/kontakt/kontakt-pagetitle.php
@@ -0,0 +1,13 @@
+<?php
+	$h1 = 'Kontakt';
+	$h2 = '';
+?>
+<!-- Page Title
+============================================= -->
+    <section id="page-title" class="page-title-parallax page-title-rh" style="background-image: url('images/Fotos/parallax/parallax03.jpg'); background-size: cover; padding: 120px 0;" data-bottom-top="background-position:0px 0px;" data-top-bottom="background-position:0px -300px;">
+
+        <div class="container clearfix">
+            <h2><?php echo $h2; ?></h2>
+            <h1><?php echo $h1; ?></h1>
+        </div>
+    </section><!-- #page-title end -->
